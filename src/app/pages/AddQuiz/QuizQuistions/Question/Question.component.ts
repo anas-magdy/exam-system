16135be68d4c5cf75f06dcfd63b44c6260693c01
anas-magdy <anasmagdy80@gmail.com>
2025-06-04 04:57:12 +0300

    edit exam

--- src/app/pages/AddQuiz/QuizQuistions/Question/Question.component.ts
+++ src/app/pages/AddQuiz/QuizQuistions/Question/Question.component.ts
@@ -1,12 +1,12 @@
 import { FormsModule } from '@angular/forms';
 import { Component, Input, OnInit } from '@angular/core';
 import { IQuestion } from '../../Quize.service';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-Question',
   templateUrl: './Question.component.html',
   styleUrls: ['./Question.component.css'],
-  imports: [FormsModule]
+  imports: [FormsModule, CommonModule]
 })
 export class QuestionComponent implements OnInit {
   @Input() question!: IQuestion
@@ -18,16 +18,28 @@ export class QuestionComponent implements OnInit {
   }
 
   get validChoice(): boolean {
-    return this.question.Choices.length < 4 &&
-      this.question.Choices[this.question.Choices.length - 1]?.value !== '';
+    return this.question.options.length < 4 &&
+      this.question.options[this.question.options.length - 1]?.option !== '';
   }
   handelAddChoice() {
-    console.log(this.question.Choices)
-    if (this.question.Choices.length < 4 && this.question.Choices[this.question.Choices.length - 1].value != '') {
-      this.question.Choices.push({
-        key: String.fromCharCode(65 + this.question.Choices.length),
-        value: ''
+    console.log(this.question.options)
+    if (this.question.options.length < 4 && this.question.options[this.question.options.length - 1].option != '') {
+      this.question.options.push({
+        key: String.fromCharCode(65 + this.question.options.length),
+        option: '',
+        isCorrect: false
       })
     }
+  }
+  getCorrectAnswerKey(): string | null {
+    const correct = this.question.options.find(opt => opt.isCorrect);
+    return correct ? correct.key : null;
+  }
+  onCorrectAnswerChange(selectedKey: string) {
+
+    console.log(selectedKey)
+    this.question.options.forEach(option => {
+      option.isCorrect = option.key === selectedKey;
+    });
   }
 }
---
+++ src/app/pages/EditQuiz/EditQuiz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-EditQuiz',
+  templateUrl: './EditQuiz.component.html',
+  styleUrls: ['./EditQuiz.component.css']
+})
+export class EditQuizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
