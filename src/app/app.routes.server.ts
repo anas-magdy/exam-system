8b87037ed906e2705e5041073ab2007c441ed250
anas-magdy <anasmagdy80@gmail.com>
2025-06-11 01:00:39 +0300

    build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,29 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // استثناء كل الصفحات الديناميكية
+  {
+    path: 'teachers/:teacherId/exams',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'exam/:examId',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'exam-result/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'view-quiz/:id/:length',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'editQuiz/:id',
+    renderMode: RenderMode.Client
+  },
+
+  // باقي الصفحات تستخدم prerender عادي
   {
     path: '**',
     renderMode: RenderMode.Prerender
